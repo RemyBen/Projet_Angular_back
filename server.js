30f63017a302206e3e059950b1c1e77170418034
Rémy BENALOUANE <pntdu83@gmail.com>
2022-12-29 18:02:50 +0100

    test heroku

--- server.js
+++ server.js
@@ -58,6 +58,7 @@ app.route(prefix + '/assignments')
 // On démarre le serveur
 app.listen(port, "0.0.0.0");
 console.log('Serveur démarré sur http://localhost:' + port);
+console.log('Serveur Fonctionne');
 
 module.exports = app;
 
