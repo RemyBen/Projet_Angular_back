593a6de29036e21034a864a2588be739d6df5338
julien bletry <julien.bletry@etu.univ-cotedazur.fr>
2023-01-03 18:49:56 +0100

    pagination

--- routes/assignments.js
+++ routes/assignments.js
@@ -1,5 +1,6 @@
 let Assignment = require('../model/assignment');
 
+/*
 // Récupérer tous les assignments (GET)
 function getAssignments(req, res){
     Assignment.find((err, assignments) => {
@@ -10,6 +11,25 @@ function getAssignments(req, res){
         res.send(assignments);
     });
 }
+*/
+// Récupérer tous les assignments (GET) pour la pagination
+function getAssignments(req, res) {
+    var aggregateQuery = Assignment.aggregate();
+
+    Assignment.aggregatePaginate(aggregateQuery,
+      {
+        page: parseInt(req.query.page) || 1,
+        limit: parseInt(req.query.limit) || 10,
+      },
+      (err, assignments) => {
+        if (err) {
+          res.send(err);
+        }
+        res.send(assignments);
+      }
+    );
+   }
+   
 
 // Récupérer un assignment par son id (GET)
 function getAssignment(req, res){
